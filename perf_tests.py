da2ec428d1f6c18d858baf64c79660ecbb6cd2ed
Dustin Schoenbrun <dustin.schoenbrun@anchore.com>
2022-01-05 10:46:34 -0500

    Add the new Asyncio Amazon Driver to Perf Test Harness
    Signed-off-by: Dustin Schoenbrun <dustin.schoenbrun@anchore.com>

--- perf_tests.py
+++ perf_tests.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 
 import amazon as async_amazon
+import amazon2 as new_amazon
 
 from anchore_enterprise.services.feeds.drivers.amazon import data as legacy
 from anchore_engine.decorators import profile
@@ -31,6 +32,17 @@ async def async_feed_load():
     return summary_count
 
 
+@profile
+async def new_async_feed_load():
+    """Load the Amazon ALAS vulnerability data with the new asyncio driver."""
+    summary_count = 0
+
+    async for item in new_amazon.items():
+        summary_count += 1
+        print(f"{summary_count:03} {item}")
+    return summary_count
+
+
 if __name__ == "__main__":
     legacy_start_time = time.time()
     legacy_count = legacy_feed_load()
@@ -40,5 +52,10 @@ if __name__ == "__main__":
     async_count = asyncio.run(async_feed_load())
     async_total_time = time.time() - async_start_time
 
-    print(f"Processed {legacy_count} in {legacy_total_time:.2f} seconds.")
-    print(f"Processed {async_count} in {async_total_time:.2f} seconds.")
+    new_async_start_time = time.time()
+    new_async_count = asyncio.run(new_async_feed_load())
+    new_async_total_time = time.time() - new_async_start_time
+
+    print(f"Legacy Driver     : Processed {legacy_count} items in {legacy_total_time:.2f} seconds.")
+    print(f"Asyncio Driver    : Processed {async_count} items in {async_total_time:.2f} seconds.")
+    print(f"New Asyncio Driver: Processed {new_async_count} items in {new_async_total_time:.2f} seconds.")
